f5d22ffef8b31f46dfc66c3cfca78179642e5c8d
Gabriel Montague <montaguegabe@users.noreply.github.com>
2026-03-14 17:56:36 -0400

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/data-trace/" : "/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
